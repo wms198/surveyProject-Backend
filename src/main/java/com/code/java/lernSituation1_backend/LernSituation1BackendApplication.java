cdf4a03b85ddd28285eb7bc40485a89adbd4eee8
wms198 <wang.minshi@icloud.com>
2024-07-02 11:27:31 +0200

    initial code for Java to start application and created test controller

---
+++ src/main/java/com/code/java/lernSituation1_backend/LernSituation1BackendApplication.java
@@ -0,0 +1,13 @@
+package com.code.java.lernSituation1_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LernSituation1BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LernSituation1BackendApplication.class, args);
+	}
+
+}
