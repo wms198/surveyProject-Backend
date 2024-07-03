d97fb1a4b3a1b353abf543db768cdff2c4ce00bb
wms198 <wang.minshi@icloud.com>
2024-07-03 13:09:16 +0200

    fix the error in QuestionRepository Interface

--- src/main/java/com/code/java/lernSituation1_backend/model/repository/QuestionRepository.java
+++ src/main/java/com/code/java/lernSituation1_backend/model/repository/QuestionRepository.java
@@ -1,10 +1,9 @@
 package com.code.java.lernSituation1_backend.model.repository;
 
-
+import com.code.java.lernSituation1_backend.model.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-
-public interface QuestionRepository<Question> extends JpaRepository<Question, Long>  {
+public interface QuestionRepository extends JpaRepository<Question, Long>  {
 }
 
 
